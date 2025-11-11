33defe74cf87b93d287b4f0aec2ac17c8fe959af
eliott.schott <eliott.schott@pixacare.com>
2025-11-11 19:45:33 +0100

    Remove unused imports from header component

--- junk-jonction/src/components/header/header.component.ts
+++ junk-jonction/src/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TuiButton, TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-header',
